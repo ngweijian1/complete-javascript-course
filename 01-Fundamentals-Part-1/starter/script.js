a24f26957017fc8c0f2132d7c492054ec514b1dc
ngweijian1 <70877795+ngweijian1@users.noreply.github.com>
2022-10-08 18:10:32 +0800

    Do till section2 15

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,120 @@
+//Lecture 1
+
+// let js = 'amazing';
+// console.log(40 + 6 + 23 - 10);
+
+// let firstName = "Ng";
+
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+// firstName = "Lee";
+// console.log(firstName);
+
+// //asm 1
+// let country = "Malaysia";
+// let continent = "Asia";
+// let population = 32.7;
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+//Lecture 2
+// let javascriptIsFun = true;
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof '23');
+
+// javascriptIsFun = "false";
+// console.log(typeof javascriptIsFun);
+
+//asm 2
+// const country = "Malaysia";
+// const isIsland = false;
+// const continent = "Asia";
+// let population = 32.7;
+// let language;
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+// console.log(isIsland);
+// console.log(language);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = null;
+// console.log(year);
+// console.log(typeof year);
+
+//asm 3
+// const country = "Malaysia";
+// const continent = "Asia";
+// let population = 32.7;
+// const language = 'Mandarin';
+// language = "English";
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+// let age = 22;
+// age = 23;
+
+// const birthYear = 2002;
+
+// var job = "consultant";
+// job = "fucker";
+
+// const now  = 2022;
+// const ageWJ = 2022 - 2000;
+// const ageMS = 2022 - 2005;
+// console.log(ageWJ, ageMS);
+// console.log(ageMS * 2 + 2 ** 3 + 2);
+
+// const firstName = "Ng";
+// const lastName = "Wei Jian";
+// console.log(firstName + ' ' + lastName);
+
+// let x = 10 + 5;
+// x += 10;
+// x *= 4;
+// console.log(x);
+
+// console.log(ageWJ < ageMS);
+// console.log(18 <= ageMS);
+
+// const now = 2022;
+// const ageWJ = 2022 - 2000;
+// const ageMS = 2022 - 2005;
+
+// console.log(now - 2000 > now - 2005);
+
+// let x, y;
+// x = y = 23 - 10 - 4;
+// console.log(x, y);
+
+// const averageAge = (ageWJ + ageMS) / 2;
+// console.log(averageAge);
+
+//asm4
+
+// const country = "Malaysia";
+// const isIsland = false;
+// const continent = "Asia";
+// let population = 32.7;
+// let language;
+// let description = 'Portugal is in Europe, and its 11 million people speak portuguese';
+
+// console.log(country);
+// console.log(continent);
+// console.log(population / 2);
+// console.log(population + 1);
+// console.log(population > 6);
+// console.log(population < 33);
+// console.log(isIsland);
+// console.log(language);
