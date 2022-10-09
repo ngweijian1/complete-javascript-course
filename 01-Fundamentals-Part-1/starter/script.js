70e5deaf7ed0d092f45db65b1c838e577016029c
ngweijian1 <70877795+ngweijian1@users.noreply.github.com>
2022-10-09 20:32:50 +0800

    until asm7

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -118,3 +118,170 @@
 // console.log(population < 33);
 // console.log(isIsland);
 // console.log(language);
+
+// coding challenge 1
+// const markWeightQ1 = 78;
+// const markHeightQ1 = 1.69;
+// const johnWeightQ1 = 92;
+// const johnHeightQ1 = 1.95;
+// const markWeightQ2 = 95;
+// const markHeightQ2 = 1.88;
+// const johnWeightQ2 = 85;
+// const johnHeightQ2 = 1.76;
+// const markBMI1 = markWeightQ1 / (markHeightQ1 ** 2);
+// const markBMI2 = markWeightQ2 / (markHeightQ2 ** 2);
+// const johnBMI1 = johnWeightQ1 / (johnHeightQ1 ** 2);
+// const johnBMI2 = johnWeightQ2 / (johnHeightQ2 ** 2);
+// const compareBMI1 = markBMI1 > johnBMI1;
+// const compareBMI2 = markBMI2 > johnBMI2;
+
+// console.log("Q1");
+// console.log(1);
+// console.log("Mark Weight", markWeightQ1);
+// console.log("Mark Height", markHeightQ1);
+// console.log("John Weight", johnWeightQ1);
+// console.log("John Weight", johnHeightQ1);
+// console.log(2);
+// console.log("Mark Weight", markWeightQ2);
+// console.log("Mark Height", markHeightQ2);
+// console.log("John Weight", johnWeightQ2);
+// console.log("John Weight", johnHeightQ2);
+// console.log("--------------------------------------");
+// console.log("Q2");
+// console.log('Mark BMI 1', markBMI1);
+// console.log('Mark BMI 2', markBMI2);
+// console.log('John BMI 1', johnBMI1);
+// console.log('John BMI 2', johnBMI2);
+// console.log("--------------------------------------");
+// console.log("Q3");
+// console.log(compareBMI1);
+// console.log(compareBMI2);
+
+// const firstName = 'Ng';
+// const job = 'consultant';
+// const birthYear = 2000;
+// const year = 2022;
+
+// const weijian = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + job + '.';
+
+// console.log(weijian);
+
+// const weijianNew = `I'm ${firstName}, a ${(year - birthYear)} years old ${job}.`;
+// console.log(weijianNew);
+
+// asm 5
+
+// const country = "Malaysia";
+// const isIsland = false;
+// const continent = "Asia";
+// let population = 32.7;
+// const language = 'Mandarin';
+// let description = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
+// console.log(description);
+
+
+// const age = 12;
+// const isOldEnought = age >= 18;
+
+// if (isOldEnought) {
+//     console.log('Old 😘');
+// } else {
+//     const yearLeft = 18 - age;
+//     console.log(`youger ${yearLeft}`);
+// }
+
+// const birthYear = 2000;
+// let century;
+// if (birthYear <= 2000) {
+//     century = 20;
+// } else {
+//     century = 21;
+// }
+// console.log(century);
+
+//asm 6
+
+// const country = "Malaysia";
+// const isIsland = false;
+// const continent = "Asia";
+// let population = 32;
+// const language = 'Mandarin';
+// const average = 33;
+// let description = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
+// console.log(description);
+
+// if (population >= average) {
+//     console.log(`${country}'s is above average`);
+// } else {
+//     const gap = average - population;
+//     console.log(`${country}'s population is ${gap} million below average`);
+// }
+
+//coding challenge #2
+// const markWeightQ1 = 78;
+// const markHeightQ1 = 1.69;
+// const johnWeightQ1 = 92;
+// const johnHeightQ1 = 1.95;
+// const markWeightQ2 = 95;
+// const markHeightQ2 = 1.88;
+// const johnWeightQ2 = 85;
+// const johnHeightQ2 = 1.76;
+// const markBMI1 = markWeightQ1 / (markHeightQ1 ** 2);
+// const markBMI2 = markWeightQ2 / (markHeightQ2 ** 2);
+// const johnBMI1 = johnWeightQ1 / (johnHeightQ1 ** 2);
+// const johnBMI2 = johnWeightQ2 / (johnHeightQ2 ** 2);
+// const compareBMI1 = markBMI1 > johnBMI1;
+// const compareBMI2 = markBMI2 > johnBMI2;
+
+// console.log("Q1");
+// console.log(1);
+// console.log("Mark Weight", markWeightQ1);
+// console.log("Mark Height", markHeightQ1);
+// console.log("John Weight", johnWeightQ1);
+// console.log("John Weight", johnHeightQ1);
+// console.log(2);
+// console.log("Mark Weight", markWeightQ2);
+// console.log("Mark Height", markHeightQ2);
+// console.log("John Weight", johnWeightQ2);
+// console.log("John Weight", johnHeightQ2);
+// console.log("--------------------------------------");
+// console.log("Q2");
+// console.log('Mark BMI 1', markBMI1);
+// console.log('Mark BMI 2', markBMI2);
+// console.log('John BMI 1', johnBMI1);
+// console.log('John BMI 2', johnBMI2);
+// console.log("--------------------------------------");
+// console.log("Q3");
+// console.log(compareBMI1);
+// console.log(compareBMI2);
+
+// if (markBMI1 > johnBMI1) {
+//     console.log(`Mark's(${markBMI1}) BMI is higher than John's(${johnBMI1}).`);
+// } else {
+//     console.log(`John's(${johnBMI1}) BMI is higher than Mark's(${markBMI1}).`);
+// }
+
+// if (markBMI2 > johnBMI2) {
+//     console.log(`Mark's(${markBMI2}) BMI is higher than John's(${johnBMI2}).`);
+// } else {
+//     console.log(`John's(${johnBMI2}) BMI is higher than Mark's(${markBMI2}).`);
+// }
+
+// const inputYear = '1991';
+// console.log(Number(inputYear) + 22);
+// console.log(inputYear + 22);
+
+// // important*
+// let n = '1' + 1; //11
+// n = n - 1; // 11 - 1
+// console.log(n); // 10
+
+//asm 7
+
+// console.log('9' - '5');
+// console.log('19' - '13' + '17');
+// console.log('19' - '13' + 17);
+// console.log('123' < 57);
+// console.log(5 + 6 + '4' + 9 - 4 - 2);
+
+
